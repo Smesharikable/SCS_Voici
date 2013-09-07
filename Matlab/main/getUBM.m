f8afd72f82aaac2672128c8928f1d6e74bd0adb6
Abramov Ivan <airvan21@gmail.com>
2013-09-07 23:37:00 +0400

    Convenient testing class for recorded testing data

--- Matlab/main/getUBM.m
+++ Matlab/main/getUBM.m
@@ -1,8 +1,8 @@
 % UBM model creation
 % Input dataMatrix - big amount of feature vectors
-% which helps to create an general model
-function [ubm] = getUBM(dataMatrix, numComponents)
+% for general model creation
+function [UBM] = getUBM(dataMatrix, numComponents)
     options = statset('Display','final');
-    ubm = gmdistribution.fit(dataMatrix, numComponents, 'CovType', 'diagonal', 'SharedCov', false,'Regularize', ...
+    UBM = gmdistribution.fit(dataMatrix, numComponents, 'CovType', 'diagonal', 'SharedCov', false,'Regularize', ...
         0.01,'Options', options);
 end
